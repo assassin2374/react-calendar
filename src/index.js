78a8a3d3164dc4eeb3951db13e436682e141ffae
assassin_box <assassin543@outlook.jp>
2021-01-18 00:34:05 +0900

    オブジェクト追加

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const ymdData=[
+  {
+    day:1,
+  },
+  {
+    day:2,
+  },
+  {
+    day:3,
+  }
+];
+
+// const month(()=>{
+//   const day=31;
+//   push
+//   console.log(day);
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>1/6</h1>
+    <div>1/17</div>
+    <div>{ymdData}</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
