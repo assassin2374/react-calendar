6f510225f6a656782d20119301aaf086f52a9324
assassin_box <assassin543@outlook.jp>
2021-01-21 00:27:08 +0900

    axiosでデータベースのスケジュール表示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 const sampleData=[
   {
@@ -17,6 +18,14 @@ const sampleData=[
   }
 ];
 
+const sampleUser=[
+  {
+    id:0,
+    name:'matsumoto',
+    pass:'passward'
+  }
+];
+
 // const month(()=>{
 //   const day=31;
 //   push
@@ -24,14 +33,22 @@ const sampleData=[
 // });
 
 const App = () => {
-  const [ymdData, setYmdData] = useState(sampleData)
+  const [schedules, setSchedules] = useState([]);
+  const [user, setUser] = useState(sampleUser);
+
+  useEffect(()=>{
+    const getSchedule = async()=>{
+      const response = await axios.get('http://localhost:4000');
+      setSchedules(response.data);
+    };
+    getSchedule();
+  }, [setSchedules]);
 
   return (
     <div>
-      <div>{ymdData[0].mon}</div>
-      {ymdData.map((dayLoop)=>{
+      {schedules.map((schedule)=>{
         return(
-          <div>{dayLoop.day}</div>
+          <div key={schedule.id}>{schedule.year}/{schedule.month}/{schedule.day}/{schedule.contents}</div>
         )
       })}
     </div>
