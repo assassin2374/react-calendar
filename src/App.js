bae330004e111da7fd0f42ddf43fbb340c078bf6
assassin_box <assassin543@outlook.jp>
2021-01-23 01:01:30 +0900

    CalendarとScheduleファイル追加

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 import React from 'react';
+import { Calendar } from "./Calendar";
+import { Schedule } from "./Schedule";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 
-const sampleData=[
-  {
-    mon:1,
-    day:17,
-  },
-  {
-    mon:1,
-    day:18,
-  },
-  {
-    mon:1,
-    day:19,
-  }
-];
-
-const sampleUser=[
-  {
-    id:0,
-    name:'matsumoto',
-    pass:'passward'
-  }
-];
-
-// const month(()=>{
-//   const day=31;
-//   push
-//   console.log(day);
-// });
+axios.defaults.baseURL = 'http://localhost:4000/'
 
 const App = () => {
-  const [schedules, setSchedules] = useState([]);
-  const [user, setUser] = useState(sampleUser);
-
-  useEffect(()=>{
-    const getSchedule = async()=>{
-      const response = await axios.get('http://localhost:4000');
-      setSchedules(response.data);
-    };
-    getSchedule();
-  }, [setSchedules]);
-
   return (
     <div>
-      {schedules.map((schedule)=>{
-        return(
-          <div key={schedule.id}>{schedule.year}/{schedule.month}/{schedule.day}/{schedule.contents}</div>
-        )
-      })}
+        <Router>
+          <Route exact path="/" component={Calendar} />
+          <Route path="/edit/:id" component={Schedule} />
+        </Router>
     </div>
   );
 }
---
+++ src/Schedule.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import './Schedule.css';
+import React from 'react';
+import axios from 'axios';
+
+const sampleData=[
+  {
+    mon:1,
+    day:17,
+  },
+  {
+    mon:1,
+    day:18,
+  },
+  {
+    mon:1,
+    day:19,
+  }
+];
+
+const sampleUser=[
+  {
+    id:0,
+    name:'matsumoto',
+    pass:'passward'
+  }
+];
+
+// const month(()=>{
+//   const day=31;
+//   push
+//   console.log(day);
+// });
+
+const Schedule = () => {
+  const [schedules, setSchedules] = useState([]);
+  const [user, setUser] = useState(sampleUser);
+
+  useEffect(()=>{
+    const getSchedule = async()=>{
+      const response = await axios.get('http://localhost:4000');
+      setSchedules(response.data);
+    };
+    getSchedule();
+  }, [setSchedules]);
+
+  return (
+    <div>
+      {schedules.map((schedule)=>{
+        return(
+          <div key={schedule.id}>{schedule.year}/{schedule.month}/{schedule.day}/{schedule.contents}</div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default Schedule;
